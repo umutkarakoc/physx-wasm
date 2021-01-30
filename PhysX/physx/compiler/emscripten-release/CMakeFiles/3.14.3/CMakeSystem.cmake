c13ba28bd31dc58d82368170518252a0cbe80e4c
umutkarakoc <umut@sent.com>
2021-01-30 21:36:17 +0300

    filter

--- PhysX/physx/compiler/emscripten-release/CMakeFiles/3.14.3/CMakeSystem.cmake
+++ PhysX/physx/compiler/emscripten-release/CMakeFiles/3.14.3/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-5.4.0-51-generic")
+set(CMAKE_HOST_SYSTEM "Linux-4.19.104-linuxkit")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "5.4.0-51-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "4.19.104-linuxkit")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/emsdk_portable/emscripten/sdk/cmake/Modules/Platform/Emscripten.cmake")
--- make.sh
+++ make.sh
@@ -3,5 +3,4 @@
 cd PhysX/physx/compiler/emscripten-release/
 make
 mkdir -p /src/lib
-cp /src/PhysX/physx/bin/emscripten/release/physx.release.js /src/dist/physx.release.js
-cp /src/PhysX/physx/bin/emscripten/release/physx.release.wasm /src/dist/physx.release.wasm
+
